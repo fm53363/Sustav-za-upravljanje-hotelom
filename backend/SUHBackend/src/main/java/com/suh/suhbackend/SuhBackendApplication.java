3be3070295f49bd62a96bd05c75daa5ea998b22d
Karlo Borovcak <borovcak.karlo@gmail.com>
2024-05-18 11:44:11 +0200

    Gitignore added

---
+++ backend/SUHBackend/src/main/java/com/suh/suhbackend/SuhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.suh.suhbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhBackendApplication.class, args);
+	}
+
+}
---
+++ backend/SUHBackend/src/test/java/com/suh/suhbackend/SuhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suh.suhbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
